3f33bdb188fa97a42292ed2e63e8b8aeda9c1097
Ido Livne <Livneido1@gmail.com>
2022-04-21 02:59:34 +0300

    maven project + spring boot

--- src/java/test/UnitTests/HistoryUnitTest.java
+++ src/java/test/UnitTests/HistoryUnitTest.java
@@ -12,6 +12,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 
+
 import java.util.HashMap;
 
 @ExtendWith(MockitoExtension.class)
