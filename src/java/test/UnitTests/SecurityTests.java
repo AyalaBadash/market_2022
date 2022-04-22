df98c152d6aa7f380dbc79cac73b561fe8415c42
razbam <razbam@post.bgu.ac.il>
2022-04-22 09:47:54 +0300

    Member login done . Need to do testing

--- src/java/test/UnitTests/SecurityTests.java
+++ src/java/test/UnitTests/SecurityTests.java
@@ -8,18 +8,26 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ExtendWith(MockitoExtension.class)
 public class SecurityTests extends mainTest{
     @Mock
     LoginCard card;
     String userName;
+
     @Test
     @DisplayName("Security Tests - No such user")
     public void testNoSuchUser(){
         Mockito.when(userName).thenReturn("moshe");
         Assertions.assertEquals("moshe",userName);
         System.out.println("1");
+        Map<String,LoginCard> userInfo = new HashMap<>();
+        userInfo.put("raz",card);
+        Assertions.assertFalse(userInfo.containsKey(userName));
     }
 
 }
