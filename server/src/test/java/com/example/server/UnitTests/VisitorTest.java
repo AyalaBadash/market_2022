f44ee0321f303967d8fd74656bef1126de5c7ef2
Ido Livne <Livneido1@gmail.com>
2022-04-26 09:09:43 +0300

    finished VisitorTest

--- server/src/test/java/com/example/server/UnitTests/VisitorTest.java
+++ server/src/test/java/com/example/server/UnitTests/VisitorTest.java
@@ -1,14 +1,16 @@
 package com.example.server.UnitTests;
 
-import com.example.server.businessLayer.Item;
-import com.example.server.businessLayer.MarketException;
-import com.example.server.businessLayer.ShoppingCart;
+import com.example.server.businessLayer.*;
 import com.example.server.businessLayer.Users.Member;
 import com.example.server.businessLayer.Users.Visitor;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+
+import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -51,46 +53,47 @@ class VisitorTest {
         assert false;
     }
 
+
+    //TODO method from visitor
     @Test
     void addToShoppingCart() {
-        Item item = Mockito.mock(Item.class);
         assert false;
 //        Mockito.when(cart.addItem()).thenReturn()
     }
 
+    // TODO remove from visitor
     @Test
     void displayShoppingCart() {
+        assert false;
     }
-
+    // TODO remove from visitor
     @Test
     void purchase() {
+        assert false;
     }
 
     @Test
     void getName() {
+        Assertions.assertEquals(visitor.getName(),name);
     }
 
     @Test
-    void setName() {
-    }
-
-    @Test
+    @DisplayName("get and set member")
     void getMember() {
+        member = Mockito.mock(Member.class);
+        ShoppingBasket shoppingBasket =  Mockito.mock(ShoppingBasket.class);
+        Shop shop = Mockito.mock(Shop.class);
+        ShoppingCart memberCart = Mockito.mock(ShoppingCart.class);
+        HashMap<Shop, ShoppingBasket> memberCartInfo =  new HashMap<>();
+        memberCartInfo.put(shop,shoppingBasket);
+        Mockito.when(memberCart.getCart()).thenReturn(memberCartInfo);
+        Assertions.assertNull(visitor.getMember());
+        Assertions.assertEquals(visitor.getCart().getCart().size(),0 );
+        visitor.setMember(member);
+        Assertions.assertEquals(visitor.getMember(),member );
+        Assertions.assertEquals(visitor.getCart().getCart().size(),1 );
+
     }
 
-    @Test
-    void setMember() {
-    }
 
-    @Test
-    void getCart() {
-    }
-
-    @Test
-    void setCart() {
-    }
-
-    @Test
-    void testEquals() {
-    }
 }
