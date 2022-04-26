a606dcf87076b6d0ed64a6fe74804265aa78517b
razbam <razbam@post.bgu.ac.il>
2022-04-26 14:39:07 +0300

    Starting testing.

--- server/src/test/java/com/example/server/UnitTests/SecurityTests.java
+++ server/src/test/java/com/example/server/UnitTests/SecurityTests.java
@@ -4,19 +4,6 @@ import com.example.server.businessLayer.LoginCard;
 import org.mockito.Mock;
 
 public class SecurityTests {
-    @Mock
-    LoginCard card;
-    String userName;
 
-//    @Test
-//    @DisplayName("Security Tests - No such user")
-//    public void testNoSuchUser(){
-//        Mockito.when(userName).thenReturn("moshe");
-//        Assertions.assertEquals("moshe",userName);
-//        System.out.println("1");
-//        Map<String,LoginCard> userInfo = new HashMap<>();
-//        userInfo.put("raz",card);
-//        Assertions.assertFalse(userInfo.containsKey(userName));
-//    }
 
 }
