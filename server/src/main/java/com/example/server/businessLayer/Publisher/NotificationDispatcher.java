9ae2bd172104793d3c78284093fc504b899f7fce
BARDAMRII <104419086+BARDAMRII@users.noreply.github.com>
2022-05-24 17:00:32 +0300

    Update NotificationDispatcher.java

--- server/src/main/java/com/example/server/businessLayer/Publisher/NotificationDispatcher.java
+++ server/src/main/java/com/example/server/businessLayer/Publisher/NotificationDispatcher.java
@@ -29,7 +29,7 @@ public class NotificationDispatcher {
         }
         return notificationDispatcher;
     }
-    private NotificationDispatcher() {
+    public NotificationDispatcher() {
         messages=new ConcurrentHashMap<>();
     }
 
