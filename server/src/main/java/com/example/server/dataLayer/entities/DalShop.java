5ec17c762b254a141b18ec420c1990b3a1c90325
dollshak <dollshak@post.bgu.ac.il>
2022-06-05 15:51:57 +0300

    map<Item, amount> -> table items_in_shop

--- server/src/main/java/com/example/server/dataLayer/entities/DalShop.java
+++ server/src/main/java/com/example/server/dataLayer/entities/DalShop.java
@@ -1,7 +1,5 @@
 package com.example.server.dataLayer.entities;
 
-import com.example.server.businessLayer.Market.Item;
-
 import javax.persistence.*;
 import java.util.List;
 import java.util.Map;
@@ -12,15 +10,22 @@ public class DalShop {
     @Id
     @Column(name = "shop_name")
     private String shop_name;
-    @OneToMany(targetEntity = DalItem.class,cascade =CascadeType.ALL )
-    @JoinColumn(name = "shop",referencedColumnName = "shop_name")
-    private List<DalItem> items;
+//    @OneToMany(targetEntity = DalItem.class,cascade =CascadeType.ALL )
+//    @JoinColumn(name = "shop",referencedColumnName = "shop_name")
+//    private List<DalItem> items;
 
     //TODO add list of managers and list of owners
 //    @OneToMany(targetEntity = DalItem.class, cascade = CascadeType.ALL)
-//    @JoinColumn(name = "shop_name", referencedColumnName = "id")
-//    @MapKeyJoinColumn (name = "basket_id")
-//    private Map<Item, Double> itemsCurrentAmount;
+//    @JoinColumn(name = "shop", referencedColumnName = "shop_name")
+//    @Column(name = "amount_in_shop")
+//    @MapKey (name = "item_id")
+
+
+    @ElementCollection
+    @CollectionTable(name = "items_in_shop", joinColumns = {@JoinColumn(name = "shop_name")})
+    @Column(name = "amount")
+    @MapKeyJoinColumn(name = "item_id")
+    private Map<DalItem, Double> itemsCurrentAmount;
     private boolean closed;
     private int rnk;
     private int rnkrs;
@@ -36,20 +41,20 @@ public class DalShop {
 
     public DalShop(String name, List<DalItem> items, List<Double> itemCurrentAmount, boolean closed, int rnk, int rnkrs) {
         this.shop_name = name;
-        this.items = items;
+//        this.items = items;
 //        this.itemCurrentAmount = itemCurrentAmount;
         this.closed = closed;
         this.rnk = rnk;
         this.rnkrs = rnkrs;
     }
-
-    public List<DalItem> getItems() {
-        return items;
-    }
-
-    public void setItems(List<DalItem> items) {
-        this.items = items;
-    }
+//
+//    public List<DalItem> getItems() {
+//        return items;
+//    }
+//
+//    public void setItems(List<DalItem> items) {
+//        this.items = items;
+//    }
 
     public String getName() {
         return shop_name;
