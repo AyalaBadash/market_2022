3b5088f69771d2962df01a8b50e83d87b921f546
Ayala <ayalabad@post.bgu.ac.il>
2022-06-07 10:18:23 +0300

    added facade objects

---
+++ server/src/main/java/com/example/server/serviceLayer/FacadeObjects/PolicyFacade/AtMostPurchasePolicyTypeFacade.java
@@ -0,0 +1,12 @@
+package com.example.server.serviceLayer.FacadeObjects.PolicyFacade;
+
+import com.example.server.businessLayer.Market.Policies.PurchasePolicy.PurchasePolicyType;
+import com.example.server.businessLayer.Market.ResourcesObjects.MarketException;
+import com.example.server.serviceLayer.FacadeObjects.PurchasePolicyTypeFacade;
+
+public class AtMostPurchasePolicyTypeFacade extends PurchasePolicyTypeFacade {
+    @Override
+    public PurchasePolicyType toBusinessObject() throws MarketException {
+        return null;
+    }
+}
