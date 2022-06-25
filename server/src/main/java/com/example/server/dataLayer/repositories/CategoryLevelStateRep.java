81d8f1c50ac69eaf1af9f33a20058e1919015e2c
dollshak <dollshak@post.bgu.ac.il>
2022-06-25 16:26:11 +0300

    fixes

--- server/src/main/java/com/example/server/dataLayer/repositories/CategoryLevelStateRep.java
+++ server/src/main/java/com/example/server/dataLayer/repositories/CategoryLevelStateRep.java
@@ -1,6 +1,7 @@
 package com.example.server.dataLayer.repositories;
 
+import com.example.server.businessLayer.Market.Policies.DiscountPolicy.DiscountState.CategoryLevelState;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryLevelStateRep extends JpaRepository<CategoryLevelStateRep, Long> {
+public interface CategoryLevelStateRep extends JpaRepository<CategoryLevelState, Long> {
 }
